0b5d2d06a0f519daa8a4f6b837e9828ea47d9c20
anuvab-b <anuvabbiswas12@gmail.com>
2023-11-09 09:55:50 +0530

    Now Playing API integrated

--- lib/data/network/api_endpoints.dart
+++ lib/data/network/api_endpoints.dart
@@ -3,4 +3,5 @@ class ApiEndpoints{
   static const tmdbBaseUrl = "https://api.themoviedb.org/3/";
   static const tmdbApiKey = "6f32d287f896a8843bdff15d65346715";
   static const tmdbToken = "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI2ZjMyZDI4N2Y4OTZhODg0M2JkZmYxNWQ2NTM0NjcxNSIsInN1YiI6IjYwMzI5ZmUwZmFiM2ZhMDAzYmZmOTZkMSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.1mWSTLbaOR4v8c-RWYvZK0-OkbUGAGTJv7JW2Z1jdi0";
+  static const tmdbPosterPath = "https://image.tmdb.org/t/p/original/";
 }
