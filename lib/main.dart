b718edb2e94072cf390bdc1b5c41097ee805d3d2
anuvab-b <anuvabbiswas12@gmail.com>
2023-11-13 22:34:54 +0530

    Dark Theme implemented

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:my_collection/data/network/network.dart';
 import 'package:my_collection/utils/routes/route_names.dart';
 import 'package:my_collection/utils/routes/routes.dart';
+import 'package:my_collection/utils/theme_styles.dart';
+import 'package:my_collection/viewmodel/dark_theme_provider.dart';
 import 'package:my_collection/viewmodel/home_screen_provider.dart';
 import 'package:my_collection/viewmodel/login/login_provider.dart';
 import 'package:my_collection/viewmodel/movie_provider.dart';
@@ -13,29 +15,50 @@ void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
 
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
   // This widget is the root of your application.
+
+  DarkThemeProvider themeChangeProvider = DarkThemeProvider();
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  void getCurrentAppTheme() async {
+    themeChangeProvider.darkTheme =
+        await themeChangeProvider.darkThemePreference.getTheme();
+  }
+
   @override
   Widget build(BuildContext context) {
     return MultiProvider(
       providers: [
-        ChangeNotifierProvider(create: (_)=> LoginProvider()),
-        ChangeNotifierProvider(create: (_)=> SignupProvider()),
-        ChangeNotifierProvider(create: (_)=> DarkThemeProvider()),
-        ChangeNotifierProvider(create: (_)=> ApiHelper()),
-        ChangeNotifierProvider(create: (_)=> MusicProvider()),
-        ChangeNotifierProvider(create: (_)=> MovieProvider()),
-        ChangeNotifierProvider(create: (_)=> HomeScreenProvider())
+        ChangeNotifierProvider(create: (_) => LoginProvider()),
+        ChangeNotifierProvider(create: (_) => SignupProvider()),
+        ChangeNotifierProvider(create: (_) => ApiHelper()),
+        ChangeNotifierProvider(create: (_) => themeChangeProvider),
+        ChangeNotifierProvider(create: (_) => MusicProvider()),
+        ChangeNotifierProvider(create: (_) => MovieProvider()),
+        ChangeNotifierProvider(create: (_) => HomeScreenProvider())
       ],
-      child: MaterialApp(
-        title: "My Collection",
-        theme: ThemeData.light(),
-        darkTheme: ThemeData.dark(),
-        initialRoute: RouteNames.home,
-        onGenerateRoute: Routes.generateRoute,
-      ),
+      child: Consumer<DarkThemeProvider>(builder: (ctx, provider, child) {
+        return MaterialApp(
+          debugShowCheckedModeBanner: false,
+          title: "My Collection",
+          theme: Styles.themeData(provider.darkTheme, context),
+          // darkTheme: ThemeData.dark(),
+          initialRoute: RouteNames.home,
+          onGenerateRoute: Routes.generateRoute,
+        );
+      }),
     );
   }
 }
--- lib/view/home_screen.dart
+++ lib/view/home_screen.dart
@@ -4,78 +4,131 @@ import 'package:my_collection/view/books_screen.dart';
 import 'package:my_collection/view/movies_screen.dart';
 import 'package:my_collection/view/music_screen.dart';
 import 'package:my_collection/view/series_screen.dart';
+import 'package:my_collection/viewmodel/dark_theme_provider.dart';
 import 'package:my_collection/viewmodel/home_screen_provider.dart';
 import 'package:provider/provider.dart';
+
 class HomeScreen extends StatelessWidget {
   const HomeScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return ChangeNotifierProvider<HomeScreenProvider>(
-      create: (context)=>HomeScreenProvider(),
+      create: (ctx) => HomeScreenProvider(),
       child: Scaffold(
-        appBar: AppBar(),
-        body: Consumer<HomeScreenProvider>(
-            builder:
-                (context,provider,child)
-            {
-              return IndexedStack(
-                index: provider.selectedIndex,
-                children: const [
-                  BooksScreen(),
-                  MoviesScreen(),
-                  SeriesScreen(),
-                  MusicScreen()
-                ],
-              );
-            }),
-        bottomNavigationBar: Consumer<HomeScreenProvider>(
-          builder: (context,provider,child) {
-            return BottomNavigationBar(
-              type: BottomNavigationBarType.fixed,
-              showUnselectedLabels: false,
-              backgroundColor: Colors.white,
-              showSelectedLabels: false,
-              selectedLabelStyle: const TextStyle(fontSize: 8.0),
-              currentIndex: provider.selectedIndex,
-              onTap: (_)=>provider.onBottomNavIndexChanged(_,context),
-              items: [
-                BottomNavigationBarItem(
-                    icon: Column(
-                      children: [
-                        Image.asset(icBook, height: 24.0, width: 24.0),
-                        const Text("Books"),
-                      ],
-                    ),
-                    label: "Books"),
-                BottomNavigationBarItem(
-                    icon: Column(
-                      children: [
-                        Image.asset(icMovie, height: 24.0, width: 24.0),
-                        const Text("Movies"),
-                      ],
-                    ),
-                    label: "Movies"),
-                BottomNavigationBarItem(
-                    icon: Column(
-                      children: [
-                        Image.asset(icSeries, height: 24.0, width: 24.0),
-                        const Text("Series"),
-                      ],
-                    ),
-                    label: "Series"),
-                BottomNavigationBarItem(
-                    icon: Column(
-                      children: [
-                        Image.asset(icMusic, height: 24.0, width: 24.0),
-                        const Text("Music"),
-                      ],
-                    ),
-                    label: "Music")
-              ],
-            );
-          }
+        appBar: AppBar(
+          actions: [
+            PopupMenuButton(
+                // color: kSelectedListItem,
+                onSelected: (value) {
+                  // controller.onMenuItemSelected(value);
+                  if(value == 0){
+                    var themeChange = Provider.of<DarkThemeProvider>(context,listen: false);
+                    themeChange.darkTheme = !themeChange.darkTheme;
+                  }
+                },
+                offset: Offset(0.0, AppBar().preferredSize.height),
+                shape: const RoundedRectangleBorder(
+                  borderRadius: BorderRadius.only(
+                    bottomLeft: Radius.circular(16.0),
+                    bottomRight: Radius.circular(16.0),
+                    topLeft: Radius.circular(16.0),
+                    topRight: Radius.circular(16.0),
+                  ),
+                ),
+                itemBuilder: (ctx) {
+                  return [
+                    PopupMenuItem(
+                        value: 0,
+                        child: SizedBox(
+                            width: 312,
+                            child: Row(
+                              mainAxisSize: MainAxisSize.max,
+                              mainAxisAlignment: MainAxisAlignment.start,
+                              children: [
+                                Icon(Icons.change_circle_rounded,color: Theme.of(context).indicatorColor),
+                                const SizedBox(width: 12.0),
+                                const Expanded(
+                                  child: Text("Change Theme",
+                                      style: TextStyle(
+                                          fontSize: 16,
+                                          fontWeight: FontWeight.w500)),
+                                ),
+                              ],
+                            )))
+                  ];
+                }),
+          ],
         ),
+        body: Consumer<HomeScreenProvider>(builder: (context, provider, child) {
+          return IndexedStack(
+            index: provider.selectedIndex,
+            children: const [
+              BooksScreen(),
+              MoviesScreen(),
+              SeriesScreen(),
+              MusicScreen()
+            ],
+          );
+        }),
+        bottomNavigationBar:
+            Consumer<HomeScreenProvider>(builder: (context, provider, child) {
+          return BottomNavigationBar(
+            type: BottomNavigationBarType.fixed,
+            showUnselectedLabels: false,
+            backgroundColor: Colors.white,
+            showSelectedLabels: false,
+            selectedLabelStyle: const TextStyle(fontSize: 8.0),
+            currentIndex: provider.selectedIndex,
+            onTap: (_) => provider.onBottomNavIndexChanged(_, context),
+            items: [
+              BottomNavigationBarItem(
+                  icon: Column(
+                    children: [
+                      Image.asset(icBook, height: 24.0, width: 24.0),
+                      const Text("Books",
+                          style: TextStyle(
+                              fontFamily: "Poppins",
+                              fontWeight: FontWeight.w600)),
+                    ],
+                  ),
+                  label: "Books"),
+              BottomNavigationBarItem(
+                  icon: Column(
+                    children: [
+                      Image.asset(icMovie, height: 24.0, width: 24.0),
+                      const Text("Movies",
+                          style: TextStyle(
+                              fontFamily: "Poppins",
+                              fontWeight: FontWeight.w600)),
+                    ],
+                  ),
+                  label: "Movies"),
+              BottomNavigationBarItem(
+                  icon: Column(
+                    children: [
+                      Image.asset(icSeries, height: 24.0, width: 24.0),
+                      const Text("Series",
+                          style: TextStyle(
+                              fontFamily: "Poppins",
+                              fontWeight: FontWeight.w600)),
+                    ],
+                  ),
+                  label: "Series"),
+              BottomNavigationBarItem(
+                  icon: Column(
+                    children: [
+                      Image.asset(icMusic, height: 24.0, width: 24.0),
+                      const Text("Music",
+                          style: TextStyle(
+                              fontFamily: "Poppins",
+                              fontWeight: FontWeight.w900)),
+                    ],
+                  ),
+                  label: "Music")
+            ],
+          );
+        }),
       ),
     );
   }
