f9689b17d91696e80895a9cd9d45f0728c3aa527
anuvab-b <anuvabbiswas12@gmail.com>
2023-11-15 19:08:38 +0530

    Books/Music endpoints

--- lib/data/network/api_endpoints.dart
+++ lib/data/network/api_endpoints.dart
@@ -4,4 +4,12 @@ class ApiEndpoints{
   static const tmdbApiKey = "6f32d287f896a8843bdff15d65346715";
   static const tmdbToken = "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI2ZjMyZDI4N2Y4OTZhODg0M2JkZmYxNWQ2NTM0NjcxNSIsInN1YiI6IjYwMzI5ZmUwZmFiM2ZhMDAzYmZmOTZkMSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.1mWSTLbaOR4v8c-RWYvZK0-OkbUGAGTJv7JW2Z1jdi0";
   static const tmdbPosterPath = "https://image.tmdb.org/t/p/original/";
+
+  static const booksBaseUrl = "https://www.googleapis.com/books/v1/volumes";
+  static const booksApiKey = "AIzaSyAj7BO2BR8_Di3imEREnbnQpqH_JGYAkQA";
+
+  static const spotifyBaseUrl = "https://api.spotify.com/v1/";
+  static const spotifyGenerateToken = "https://accounts.spotify.com/api/token";
+  static const spotifyClientId = "7ce347773aea4c15a532fafecbf785dc";
+  static const spotifyClientSecret = "9b4b161957a54270a1695051a2c56851";
 }
