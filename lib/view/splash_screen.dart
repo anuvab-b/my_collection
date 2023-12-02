eec721609baa61e8ce1b5e3a51992c6161517c6e
anuvab-b <anuvabbiswas12@gmail.com>
2023-12-02 23:41:08 +0530

    checkIfUserLoggedIn, addUserToCollection

--- lib/view/splash_screen.dart
+++ lib/view/splash_screen.dart
@@ -12,35 +12,26 @@ class SplashScreen extends StatefulWidget {
 }
 
 class _SplashScreenState extends State<SplashScreen> {
-
   @override
-  void initState(){
+  void initState() {
     super.initState();
     checkIfUserLoggedIn();
   }
 
-  checkIfUserLoggedIn()async{
+  checkIfUserLoggedIn() async {
     AuthProvider authProvider = context.read<AuthProvider>();
     bool res = await authProvider.checkIfUserLoggedIn();
-    SchedulerBinding.instance.addPostFrameCallback((_)  {
-      if(res == true) {
-        if (context.mounted) {
-          Navigator.pushNamedAndRemoveUntil(
-              context,
-              RouteNames.home,
-                  (Route<dynamic> route) => false);
-        }
-      }
-      else{
-        if (context.mounted) {
-          Navigator.pushNamedAndRemoveUntil(
-              context,
-              RouteNames.login,
-                  (Route<dynamic> route) => false);
-        }
+    SchedulerBinding.instance.addPostFrameCallback((_) {
+      if (res == true) {
+        Navigator.pushNamedAndRemoveUntil(
+            context, RouteNames.home, (Route<dynamic> route) => false);
+      } else {
+        Navigator.pushNamedAndRemoveUntil(
+            context, RouteNames.login, (Route<dynamic> route) => false);
       }
     });
   }
+
   @override
   Widget build(BuildContext context) {
     return const SizedBox();
