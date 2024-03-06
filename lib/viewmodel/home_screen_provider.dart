e4e49982d526643550d73bedca24dfbc25ee6bbd
anuvab-b <anuvabbiswas12@gmail.com>
2024-03-06 06:17:01 +0530

    Fetch Series Home data with isolates

--- lib/viewmodel/home_screen_provider.dart
+++ lib/viewmodel/home_screen_provider.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:my_collection/viewmodel/books_provider.dart';
 import 'package:my_collection/viewmodel/movie_provider.dart';
+import 'package:my_collection/viewmodel/movie_watchlist_provider.dart';
 import 'package:my_collection/viewmodel/music_provider.dart';
 import 'package:my_collection/viewmodel/playlist_provider.dart';
 import 'package:my_collection/viewmodel/reading_list_provider.dart';
@@ -10,38 +11,40 @@ import 'package:provider/provider.dart';
 class HomeScreenProvider extends ChangeNotifier{
 
   int selectedIndex = 0;
-  onBottomNavIndexChanged(int index,BuildContext context){
+  onBottomNavIndexChanged(int index,int currentIndex, BuildContext context) {
+    if (index != currentIndex) {
+      if (index == 0) {
+        BooksProvider booksProvider = Provider.of<BooksProvider>(
+            context, listen: false);
+        ReadingListProvider readingListProvider = Provider.of<
+            ReadingListProvider>(context, listen: false);
+        booksProvider.fetchBookHomeScreenData();
+        readingListProvider.fetchReadingLists();
+      }
 
-    if(index == 0){
-      BooksProvider booksProvider = Provider.of<BooksProvider>(context,listen: false);
-      ReadingListProvider readingListProvider = Provider.of<ReadingListProvider>(context,listen: false);
-      booksProvider.fetchBookHomeScreenData();
-      readingListProvider.fetchReadingLists();
-    }
+      if (index == 1) {
+        MovieProvider movieProvider = Provider.of<MovieProvider>(context, listen: false);
+        MovieWatchListProvider movieWatchListProvider = Provider.of<MovieWatchListProvider>(context, listen: false);
 
-    if(index == 1) {
-      MovieProvider movieProvider = Provider.of<MovieProvider>(
-          context, listen: false);
-      movieProvider.getNowPlayingMovies();
-      movieProvider.getPopularMovies();
-      movieProvider.getTopRatedMovies();
-      movieProvider.getUpcomingMovies();
-    }
-    else if(index == 2){
-      SeriesProvider seriesProvider = Provider.of<SeriesProvider>(context,listen: false);
-      seriesProvider.getAiringToday();
-      seriesProvider.getOnTheAir();
-      seriesProvider.getPopular();
-      seriesProvider.getTopRated();
-    }
+        movieProvider.fetchMovieHomeData();
+        movieWatchListProvider.fetchMovieWatchlistLists();
+      }
+      else if (index == 2) {
+        SeriesProvider seriesProvider = Provider.of<SeriesProvider>(context, listen: false);
+        // TODO: fetchWatchListLists
+        seriesProvider.fetchSeriesHomeData();
+      }
 
-    else if(index == 3){
-      MusicProvider musicProvider = Provider.of<MusicProvider>(context,listen: false);
-      PlayListProvider playListProvider = Provider.of<PlayListProvider>(context,listen: false);
-      musicProvider.getToken();
-      playListProvider.fetchMusicPlayLists();
+      else if (index == 3) {
+        MusicProvider musicProvider = Provider.of<MusicProvider>(
+            context, listen: false);
+        PlayListProvider playListProvider = Provider.of<PlayListProvider>(
+            context, listen: false);
+        musicProvider.getToken();
+        playListProvider.fetchMusicPlayLists();
+      }
+      selectedIndex = index;
+      notifyListeners();
     }
-    selectedIndex = index;
-    notifyListeners();
   }
 }
