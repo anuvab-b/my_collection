936983379060f9ae43b8434b7b13e21765a6bffe
anuvab-b <anuvabbiswas12@gmail.com>
2024-03-07 09:46:24 +0530

    Updated credentials

--- lib/repository/books_repository.dart
+++ lib/repository/books_repository.dart
@@ -1,6 +1,7 @@
 import 'package:dartz/dartz.dart';
 import 'package:my_collection/data/network/api_endpoints.dart';
 import 'package:my_collection/data/network/network.dart';
+import 'package:my_collection/data/network/secrets.dart';
 import 'package:my_collection/domain/i_books_repository.dart';
 import 'package:my_collection/models/books/google_books_api_response_model.dart';
 import 'package:my_collection/models/books/google_books_detail_response_model.dart';
@@ -16,7 +17,7 @@ class BooksRepository implements IBooksRepository{
       int maxResults = 20}) async {
     GoogleBooksApiResponseModel bookApiResponseModel;
     String url =
-        "${ApiEndpoints.booksBaseUrl}?q=$query&key=${ApiEndpoints.booksApiKey}&orderBy=$orderBy&startIndex=$startIndex&maxResults=$maxResults";
+        "${ApiEndpoints.booksBaseUrl}?q=$query&key=${Secrets.booksApiKey}&orderBy=$orderBy&startIndex=$startIndex&maxResults=$maxResults";
 
     try {
       var res = await apiHelper.request(url: url, method: HTTPMETHOD.GET);
